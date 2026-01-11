aebaeaf629d6db97913876f0005dfbaf334d268d
nusry5049 <nusryrahman@gmail.com>
2026-01-12 01:30:04 +0530

    feat(assignments): add RepRouteAssignment repository

---
+++ backend/src/main/java/com/repnexa/modules/assignments/repo/RepRouteAssignmentRepository.java
@@ -0,0 +1,6 @@
+package com.repnexa.modules.assignments.repo;
+
+import com.repnexa.modules.assignments.domain.RepRouteAssignment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RepRouteAssignmentRepository extends JpaRepository<RepRouteAssignment, Long> {}
